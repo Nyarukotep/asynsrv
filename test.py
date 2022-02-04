441197af791c7e8391afefb36d247a5154ce94ec
Nyarukotep <nyarukotep@gmail.com>
2022-02-04 03:33:25 -0500

    create file

---
+++ test.py
@@ -0,0 +1,17 @@
+import asynsrv
+def asg(mfc):
+    cat={
+        1:test,
+    }
+    mtc = cat.get(1,test)
+    return mtc()
+def test():
+    str='成功'
+    str = str.encode('unicode-escape')
+    msg = b'HTTP/1.1 Hello World\r\n'\
+        b'Connection: keep-alive\r\n'\
+        b'Content-Length: 20\r\n\r\n'\
+        b'<h1>Hello World</h1>'
+    return msg
+s=asynsrv.server()
+s.start(asg)
